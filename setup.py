b27c9774bb236025daebecda74f503dbdbb5d6a4
Mohd Saqib <mohdsaqibhbi@gmail.com>
2022-03-13 13:43:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', encoding='utf-8') as readme_file:
     README = readme_file.read()
 
 setup_args = dict(
-    name='easy_images',
+    name='easy_images_downloader',
     version='0.0.1',
     description='Download hundreds of images from Google. Do image post processing later.',
     long_description=README,
@@ -15,15 +15,7 @@ setup_args = dict(
     author_email='mohdsaqibhbi@gmail.com',
     keywords=['python image download', 'google images', 'download images', 'image downloader'],
     url='https://github.com/mohdsaqibhbi/easy_images.git',
-    download_url='https://pypi.org/project/easy_images/',
-    classifiers=[
-        "Development Status :: Release",
-        "Intended Audience :: Developers, Machine Learning Engineers, Computer Vision",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
+    download_url='https://pypi.org/project/easy_images_downloader/',
 )
 
 install_requires = ['requests', 'opencv-python','python-magic','beautifulsoup4', 'selenium', 'tqdm']
