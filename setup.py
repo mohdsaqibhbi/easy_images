995cca6108f6503323d70f336d8e9e9b06ebe034
mohdsaqibhbi <mohdsaqibhbi@gmail.com>
2022-03-13 13:03:48 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name='easy_images',
+    version='0.0.1',
+    description='Download hundreds of images from Google. Do image post processing later.',
+    long_description=README,
+	long_description_content_type='text/markdown',
+    license='MIT',
+    packages=find_packages(),
+    author='Mohd Saqib',
+    author_email='mohdsaqibhbi@gmail.com',
+    keywords=['python image download', 'google images', 'download images', 'image downloader'],
+    url='https://github.com/mohdsaqibhbi/easy_images.git',
+    download_url='https://pypi.org/project/easy_images/'
+)
+
+install_requires = ['requests', 'opencv-python','python-magic','beautifulsoup4', 'selenium', 'tqdm']
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
